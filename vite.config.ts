9a43976f64144130665f089125459db9230c6e3a
myscreen999 <myscreen999@gmail.com>
2025-07-09 07:58:50 -1200

    Fix Netlify CMS publishing and 404 errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,10 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: false,
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'public/admin/index.html')
+      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
